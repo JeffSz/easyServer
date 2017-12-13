cfcaee85a8d315c218c2e2594c8cecc7814bf681
李福骄 <suizhouren@163.com>
2017-12-13 15:50:42 +0800

    删除无用文件

--- server.go
+++ server.go
@@ -1,6 +1,5 @@
 package easyServer
 
-
 import (
 	"time"
 	"regexp"
@@ -11,39 +10,44 @@ import (
 	"github.com/JeffSz/logger"
 	"strings"
 	"net"
+	"runtime/debug"
 )
+
 func timeSub(t time.Time) int64 {
 	return int64(time.Now().Sub(t) / time.Millisecond)
 }
+
 type Method int
 type ErrorHandler func(w http.ResponseWriter, error Error) error
 type RouteHandler func(http.ResponseWriter, *http.Request)
+
 var HTTPMethods = make(map[string]Method)
 
-const(
-	HTTP_GET = 0x01
-	HTTP_POST = 0x02
-	HTTP_PUT = 0x04
+const (
+	HTTP_GET    = 0x01
+	HTTP_POST   = 0x02
+	HTTP_PUT    = 0x04
 	HTTP_DELETE = 0x08
-	HTTP_ALL = 0xFF
+	HTTP_ALL    = 0xFF
 )
 
-type MyRouter struct{
-	method Method
+type MyRouter struct {
+	method  Method
 	pattern *regexp.Regexp
 	handler RouteHandler
 }
-type Server struct{
-	routes []MyRouter
+type Server struct {
+	routes       []MyRouter
 	errorHandler ErrorHandler
 }
-func (server *Server) AddRoute(url string, method Method, handler RouteHandler) error{
-	if method != HTTP_GET && method != HTTP_POST && method != HTTP_ALL{
+
+func (server *Server) AddRoute(url string, method Method, handler RouteHandler) error {
+	if method != HTTP_GET && method != HTTP_POST && method != HTTP_ALL {
 		return errors.New("method not allow")
 	}
-	if pattern, err := regexp.Compile(url); err != nil{
+	if pattern, err := regexp.Compile(url); err != nil {
 		return err
-	}else{
+	} else {
 		server.routes = append(server.routes, MyRouter{method: method, pattern: pattern, handler: handler})
 		return nil
 	}
@@ -57,19 +61,19 @@ func (server Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	startTime := time.Now()
 	defer func() {
 		if err := recover(); err != nil {
-			logger.Error(fmt.Sprintf("Error found: %s\n", stack()))
+			logger.Error(fmt.Sprintf("Error found: %s\n", debug.Stack()))
 			w.WriteHeader(http.StatusInternalServerError)
 			io.WriteString(w, "Server Error")
 			logger.Error(err)
 			logger.Error(time.Now(), "request:", r, timeSub(startTime), http.StatusInternalServerError)
 
-			if er, is := err.(Error); is{
+			if er, is := err.(Error); is {
 				server.errorHandler(w, er)
 			}
 		}
 	}()
-	for _, router := range server.routes{
-		if router.pattern.MatchString(r.URL.Path) && (router.method & HTTPMethods[strings.ToUpper(r.Method)] != 0x00){
+	for _, router := range server.routes {
+		if router.pattern.MatchString(r.URL.Path) && (router.method&HTTPMethods[strings.ToUpper(r.Method)] != 0x00) {
 			router.handler(w, r)
 			logger.Debug(time.Now(), "request:", *r, timeSub(startTime), http.StatusOK)
 			return
@@ -88,7 +92,7 @@ func init() {
 	HTTPMethods["ALL"] = HTTP_ALL
 }
 
-func LocalIP() string{
+func LocalIP() string {
 	address, err := net.InterfaceAddrs()
 
 	if err != nil {
@@ -105,11 +109,11 @@ func LocalIP() string{
 	panic("No such device")
 }
 
-func NewServer(errorHandler ErrorHandler) *Server{
-	return &Server{routes:make([]MyRouter, 0), errorHandler: errorHandler}
+func NewServer(errorHandler ErrorHandler) *Server {
+	return &Server{routes: make([]MyRouter, 0), errorHandler: errorHandler}
 }
 
-func easyHandler(w http.ResponseWriter, error Error) error{
+func easyHandler(w http.ResponseWriter, error Error) error {
 	w.WriteHeader(http.StatusBadRequest)
 	io.WriteString(w, error.Error())
 	return nil
--- utils.go
+++
@@ -1,9 +0,0 @@
-package easyServer
-
-import "runtime"
-
-func stack() []byte {
-	buf := make([]byte, 1024)
-	n := runtime.Stack(buf, false)
-	return buf[:n]
-}
